463bb99e3cc818e47108b94fbad64892863d26f1
Ssousuke <wesleysousukee@gmail.com>
2023-12-19 18:39:45 -0300

    refactor: Refatorar assinatura dos métodos na IGenericRepository<T>

--- src/LocalLibrary.Domain/IRepository/IGenericRepository.cs
+++ src/LocalLibrary.Domain/IRepository/IGenericRepository.cs
@@ -2,10 +2,10 @@
 {
     public interface IGenericRepository<T> where T : class
     {
-        public Task AddAsync(T entity);
-        public Task UpdateAsync(T entity);
+        public Task<T> AddAsync(T entity);
+        public Task<T> UpdateAsync(T entity);
         public Task<T> GetByIdAsync(Guid id);
-        public bool DeleteByIsAsync(Guid id);
+        public Task<bool> DeleteByIsAsync(Guid id);
         public Task<IEnumerable<T>> GetAll();
     }
 }
