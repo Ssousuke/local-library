2ef74d22438a64e1c9f60468c8c4416582aed934
Ssousuke <wesleysousukee@gmail.com>
2023-12-19 18:49:45 -0300

    feat: Implementa métodos da Interface IGenericRepository<Author>

--- src/LocalLibrary.Infra.Data/Repository/AuthorRepository.cs
+++ src/LocalLibrary.Infra.Data/Repository/AuthorRepository.cs
@@ -1,33 +1,89 @@
 ﻿using LocalLibrary.Domain.IRepository;
 using LocalLibrary.Domain.Models;
+using LocalLibrary.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity;
 
 namespace LocalLibrary.Infra.Data.Repository
 {
     public class AuthorRepository : IGenericRepository<Author>
     {
-        public Task AddAsync(Author entity)
+        private readonly ContextDB context;
+
+        public AuthorRepository(ContextDB context)
         {
-            throw new NotImplementedException();
+            this.context = context;
         }
 
-        public bool DeleteByIsAsync(Guid id)
+        public async Task<Author> AddAsync(Author entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await context.Authors.AddAsync(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbUpdateException($"Erro: Não foi possível atualizar ou inserir informações no banco. - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro: Ocorreu um erro ao adicionar um autor. - {ex.Message}");
+            }
         }
 
-        public Task<IEnumerable<Author>> GetAll()
+        public async Task<bool> DeleteByIsAsync(Guid id)
         {
-            throw new NotImplementedException();
+
+            var deleteId = await context.Authors.SingleAsync(x => x.Id == id);
+            if (deleteId != null)
+            {
+                try
+                {
+                    context.Authors.Remove(deleteId);
+                    await context.SaveChangesAsync();
+                    return true;
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    throw new DbUpdateConcurrencyException($"Erro: Não foi possível deletar.  está sendo utilizado por outro {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erro: Ocorreu um erro ao adicionar um autor. - {ex.Message}");
+                }
+            }
+
+            return false;
         }
 
-        public Task<Author> GetByIdAsync(Guid id)
+        public async Task<IEnumerable<Author>> GetAll()
         {
-            throw new NotImplementedException();
+            return await context.Authors.ToListAsync();
         }
 
-        public Task UpdateAsync(Author entity)
+        public async Task<Author> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await context.Authors.SingleAsync(x => x.Id == id);
+        }
+
+        public async Task<Author> UpdateAsync(Author entity)
+        {
+            try
+            {
+                context.Authors.Update(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbUpdateException($"Erro: Não foi possível atualizar ou inserir informações no banco. - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro: Ocorreu um erro ao atualizar um autor. - {ex.Message}");
+            }
         }
     }
 }
