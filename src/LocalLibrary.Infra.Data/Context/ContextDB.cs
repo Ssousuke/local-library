53e05693700b66003e1fd776769477e56ed3fde2
Ssousuke <wesleysousukee@gmail.com>
2023-12-19 20:32:45 -0300

    chore: Adicionar método OnModelCreating

--- src/LocalLibrary.Infra.Data/Context/ContextDB.cs
+++ src/LocalLibrary.Infra.Data/Context/ContextDB.cs
@@ -5,10 +5,18 @@ namespace LocalLibrary.Infra.Data.Context
 {
     public class ContextDB : DbContext
     {
-        public ContextDB() { }
+        public ContextDB(DbContextOptions<ContextDB> options) : base(options) { }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book> Books { get; set; }
         public DbSet<Genre> Genres { get; set; }
         public DbSet<Language> Languages { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ContextDB).Assembly);
+        }
     }
 }
