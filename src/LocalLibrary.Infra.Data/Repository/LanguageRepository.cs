42085a0d17ed74d681e9e726ba56a4aa9b264ea1
Ssousuke <wesleysousukee@gmail.com>
2023-12-19 18:50:17 -0300

    feat: Implementa métodos da Interface IGenericRepository<Language>

--- src/LocalLibrary.Infra.Data/Repository/LanguageRepository.cs
+++ src/LocalLibrary.Infra.Data/Repository/LanguageRepository.cs
@@ -1,33 +1,82 @@
 ﻿using LocalLibrary.Domain.IRepository;
 using LocalLibrary.Domain.Models;
+using LocalLibrary.Infra.Data.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocalLibrary.Infra.Data.Repository
 {
     public class LanguageRepository : IGenericRepository<Language>
     {
-        public Task AddAsync(Language entity)
+        private readonly ContextDB _context;
+
+        public LanguageRepository(ContextDB context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public bool DeleteByIsAsync(Guid id)
+        public async Task<Language> AddAsync(Language entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await _context.Languages.AddAsync(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbUpdateException($"Erro: Não foi possível atualizar ou inserir informações no banco. - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro: Ocorreu um erro ao adicionar language. - {ex.Message}");
+            }
         }
 
-        public Task<IEnumerable<Language>> GetAll()
+        public async Task<bool> DeleteByIsAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var language = await _context.Languages.SingleAsync(x => x.Id == id);
+            if (language != null)
+            {
+                try
+                {
+                    _context.Languages.Remove(language);
+                    await _context.SaveChangesAsync();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Erro: Não foi possível deletar registro: {ex.Message}");
+                }
+            }
+            return false;
         }
 
-        public Task<Language> GetByIdAsync(Guid id)
+        public async Task<IEnumerable<Language>> GetAll()
         {
-            throw new NotImplementedException();
+            return await _context.Languages.ToListAsync();
         }
 
-        public Task UpdateAsync(Language entity)
+        public async Task<Language> GetByIdAsync(Guid id)
         {
-            throw new NotImplementedException();
+            return await _context.Languages.SingleAsync(_ => _.Id == id);
+        }
+
+        public async Task<Language> UpdateAsync(Language entity)
+        {
+            try
+            {
+                _context.Languages.Update(entity);
+                await _context.SaveChangesAsync();
+                return entity;
+            }
+            catch (DbUpdateException ex)
+            {
+                throw new DbUpdateException($"Erro: Não foi possível atualizar ou inserir informações no banco. - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Erro: Ocorreu um erro ao adicionar language. - {ex.Message}");
+            }
         }
     }
 }
