856a0e84b12a724ce858d49e2061345206d344e1
Ssousuke <wesleysousukee@gmail.com>
2023-12-20 17:55:25 -0300

    chore: injeção de dependencia dos serviços

--- src/LocalLibrary.IoC/InfraModule.cs
+++ src/LocalLibrary.IoC/InfraModule.cs
@@ -1,4 +1,7 @@
-﻿using LocalLibrary.Application.Mappings;
+﻿using LocalLibrary.Application.DTO;
+using LocalLibrary.Application.Mappings;
+using LocalLibrary.Application.Services;
+using LocalLibrary.Application.Services.IServices;
 using LocalLibrary.Domain.IRepository;
 using LocalLibrary.Domain.Models;
 using LocalLibrary.Infra.Data.Context;
@@ -21,6 +24,11 @@ namespace LocalLibrary.IoC
             services.AddScoped(typeof(IGenericRepository<Book>), typeof(BookRepository));
             services.AddScoped(typeof(IGenericRepository<Genre>), typeof(GenreRepository));
             services.AddScoped(typeof(IGenericRepository<Language>), typeof(LanguageRepository));
+
+            services.AddScoped(typeof(IGenericServices<AuthorDTO>), typeof(AuthorServices));
+            services.AddScoped(typeof(IGenericServices<BookDTO>), typeof(BookServices));
+            services.AddScoped(typeof(IGenericServices<GenreDTO>), typeof(GenreServices));
+            services.AddScoped(typeof(IGenericServices<LanguageDTO>), typeof(LanguageServices));
 
             services.AddAutoMapper(typeof(ProfileMap));
 
