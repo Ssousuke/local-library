3628059ffcf1752535de27b3bab130ca340d48fb
Ssousuke <wesleysousukee@gmail.com>
2023-12-30 19:53:56 -0300

    feat: classe de validação propriedades e método que verifica e remove caracteres especiais

---
+++ src/LocalLibrary.Domain/ValidationDomainException/ValidateFields.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalLibrary.Domain.ValidationDomainException
+{
+    public static class ValidateFields
+    {
+        public static string CheckAndRemoveSpecialCharacters(string inputString)
+        {
+            var newString = string.Empty;
+            if (!string.IsNullOrEmpty(inputString))
+            {
+                // Caracteres a serem removidos
+                char[] caracteresEspeciais = { '-', '/', '\\', '.', ';', '@', '$' };
+
+                // Verifica e remove os caracteres especiais
+                foreach (char c in inputString)
+                {
+                    if (!caracteresEspeciais.Contains(c))
+                    {
+                        newString += c;
+                    }
+                }
+            }
+
+            return newString;
+        }
+    }
+}
