54173b646443e370b013fdb220623ec6a135becb
Ssousuke <wesleysousukee@gmail.com>
2023-12-30 19:54:38 -0300

    feat: para validação e lançamentos de exeção

---
+++ src/LocalLibrary.Domain/ValidationDomainException/DomainException.cs
@@ -0,0 +1,13 @@
+﻿namespace LocalLibrary.Domain.ValidationDomainException
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string err) : base(err) { }
+
+        public static void When(bool hasError, string error)
+        {
+            if (hasError)
+                throw new DomainException(error);
+        }
+    }
+}
