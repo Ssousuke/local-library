4696140a257b21b86504335a1aed91e2ba8fbd03
Ssousuke <wesleysousukee@gmail.com>
2024-01-20 16:31:11 -0300

    remove: RabbitMQ

--- src/LocalLibrary.API/RabbitMQSender/IRabbitMQMessageSender.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace LocalLibrary.API.RabbitMQSender
-{
-    public interface IRabbitMQMessageSender<T> where T : class
-    {
-        void SendMessage(T message, string queueName);
-        void SendMessage(IEnumerable<T> message, string queueName);
-    }
-}
