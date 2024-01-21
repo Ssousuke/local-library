f5d987692e91d0748fd6379babb64e9086bffed7
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 02:16:00 -0300

    add mapper for commands

--- src/LocalLibrary.Application/Mappings/ProfileMap.cs
+++ src/LocalLibrary.Application/Mappings/ProfileMap.cs
@@ -1,4 +1,8 @@
 ﻿using AutoMapper;
+using LocalLibrary.Application.CQRS.Author.Commands;
+using LocalLibrary.Application.CQRS.Book.Commands;
+using LocalLibrary.Application.CQRS.Genre.Commands;
+using LocalLibrary.Application.CQRS.Language.Commands;
 using LocalLibrary.Application.DTO;
 using LocalLibrary.Domain.Models;
 
@@ -12,6 +16,22 @@ namespace LocalLibrary.Application.Mappings
             CreateMap<BookDTO, Book>().ReverseMap();
             CreateMap<GenreDTO, Genre>().ReverseMap();
             CreateMap<Language, LanguageDTO>().ReverseMap();
+
+            CreateMap<AuthorDTO, AuthorCreateCommand>().ReverseMap();
+            CreateMap<AuthorDTO, AuthorRemoveCommand>().ReverseMap();
+            CreateMap<AuthorDTO, AuthorUpdateCommand>().ReverseMap();
+
+            CreateMap<BookDTO, BookCreateCommand>().ReverseMap();
+            CreateMap<BookDTO, BookRemoveCommand>().ReverseMap();
+            CreateMap<BookDTO, BookUpdateCommand>().ReverseMap();
+
+            CreateMap<BookDTO, GenreCreateCommand>().ReverseMap();
+            CreateMap<BookDTO, GenreRemoveCommand>().ReverseMap();
+            CreateMap<BookDTO, GenreUpdateCommand>().ReverseMap();
+
+            CreateMap<BookDTO, LanguageCreateCommand>().ReverseMap();
+            CreateMap<BookDTO, LanguageRemoveCommand>().ReverseMap();
+            CreateMap<BookDTO, LanguageUpdateCommand>().ReverseMap();
         }
     }
 }
