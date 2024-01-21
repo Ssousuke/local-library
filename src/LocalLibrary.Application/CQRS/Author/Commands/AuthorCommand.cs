6840bff4830b4a968b08402aadb8c14fed62282d
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 05:43:39 -0300

    chore: add Id

--- src/LocalLibrary.Application/CQRS/Author/Commands/AuthorCommand.cs
+++ src/LocalLibrary.Application/CQRS/Author/Commands/AuthorCommand.cs
@@ -4,6 +4,7 @@ namespace LocalLibrary.Application.CQRS.Author.Commands
 {
     public abstract class AuthorCommand : IRequest<Domain.Models.Author>
     {
+        public Guid Id { get; set; }
         public string Name { get; private set; }
         public DateTime DateOfBirth { get; private set; }
         public DateTime? DateOfDeath { get; private set; }
