9f9d297ee465160a46a5d55c099bdf537e3efe29
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 05:44:22 -0300

    chore : AuthorCommand to IRequest<Domain.Models.Author>

--- src/LocalLibrary.Application/CQRS/Author/Commands/AuthorCreateCommand.cs
+++ src/LocalLibrary.Application/CQRS/Author/Commands/AuthorCreateCommand.cs
@@ -1,6 +1,11 @@
-﻿namespace LocalLibrary.Application.CQRS.Author.Commands
+﻿using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Author.Commands
 {
-    public class AuthorCreateCommand : AuthorCommand
+    public class AuthorCreateCommand : IRequest<Domain.Models.Author>
     {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfDeath { get; set; }
     }
 }
