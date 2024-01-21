2a075e2f613009b84ec0240f4c5d202ab69687d7
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 02:16:46 -0300

    add query book handler

---
+++ src/LocalLibrary.Application/CQRS/Book/Handler/GetBookByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LocalLibrary.Application.CQRS.Book.Queries;
+using LocalLibrary.Domain.IRepository;
+using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Book.Handler
+{
+    public class GetBookByIdQueryHandler : IRequestHandler<GetBookByIdQuery, Domain.Models.Book>
+    {
+        private readonly IGenericRepository<Domain.Models.Book> _booksRepository;
+
+        public GetBookByIdQueryHandler(IGenericRepository<Domain.Models.Book> booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        public async Task<Domain.Models.Book> Handle(GetBookByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _booksRepository.GetByIdAsync(request.BookId);
+        }
+    }
+}
---
+++ src/LocalLibrary.Application/CQRS/Book/Handler/GetBooksQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LocalLibrary.Application.CQRS.Book.Queries;
+using LocalLibrary.Domain.IRepository;
+using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Book.Handler
+{
+    public class GetBooksQueryHandler : IRequestHandler<GetBooksQuery, IEnumerable<Domain.Models.Book>>
+    {
+        private readonly IGenericRepository<Domain.Models.Book> _booksRepository;
+
+        public GetBooksQueryHandler(IGenericRepository<Domain.Models.Book> booksRepository)
+        {
+            _booksRepository = booksRepository;
+        }
+
+        public async Task<IEnumerable<Domain.Models.Book>> Handle(GetBooksQuery request, CancellationToken cancellationToken)
+        {
+            return await _booksRepository.GetAll();
+        }
+    }
+}
