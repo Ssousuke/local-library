6829fbd586a134f0b972497b3f9d3d453533ea5d
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 02:17:33 -0300

    add query language handler

---
+++ src/LocalLibrary.Application/CQRS/Language/Handler/GetLanguageByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LocalLibrary.Application.CQRS.Language.Queries;
+using LocalLibrary.Domain.IRepository;
+using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Language.Handler
+{
+    public class GetLanguageByIdQueryHandler : IRequestHandler<GetLanguageByIdQuery, Domain.Models.Language>
+    {
+        private readonly IGenericRepository<Domain.Models.Language> _languageRepository;
+
+        public GetLanguageByIdQueryHandler(IGenericRepository<Domain.Models.Language> languageRepository)
+        {
+            _languageRepository = languageRepository;
+        }
+
+        public async Task<Domain.Models.Language> Handle(GetLanguageByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _languageRepository.GetByIdAsync(request.LanguageId);
+        }
+    }
+}
---
+++ src/LocalLibrary.Application/CQRS/Language/Handler/GetLanguagesQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LocalLibrary.Application.CQRS.Language.Queries;
+using LocalLibrary.Domain.IRepository;
+using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Language.Handler
+{
+    public class GetLanguagesQueryHandler : IRequestHandler<GetLanguageQuery, IEnumerable<Domain.Models.Language>>
+    {
+        private readonly IGenericRepository<Domain.Models.Language> _languageRepository;
+
+        public GetLanguagesQueryHandler(IGenericRepository<Domain.Models.Language> languageRepository)
+        {
+            _languageRepository = languageRepository;
+        }
+
+        public async Task<IEnumerable<Domain.Models.Language>> Handle(GetLanguageQuery request, CancellationToken cancellationToken)
+        {
+            return await _languageRepository.GetAll();
+        }
+    }
+}
