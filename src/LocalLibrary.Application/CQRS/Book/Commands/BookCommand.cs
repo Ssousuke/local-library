32470a2e7c103366e2e61b0d2a256e363d31ea7b
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 05:44:53 -0300

    chore: add Id

--- src/LocalLibrary.Application/CQRS/Book/Commands/BookCommand.cs
+++ src/LocalLibrary.Application/CQRS/Book/Commands/BookCommand.cs
@@ -4,11 +4,12 @@ namespace LocalLibrary.Application.CQRS.Book.Commands
 {
     public abstract class BookCommand : IRequest<Domain.Models.Book>
     {
-        public string Title { get; private set; }
-        public string Summary { get; private set; }
-        public string ISBN { get; private set; }
-        public Guid AuthorId { get; private set; }
-        public Guid GenreId { get; private set; }
-        public Guid LanguageId { get; private set; }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string ISBN { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid GenreId { get; set; }
+        public Guid LanguageId { get; set; }
     }
 }
