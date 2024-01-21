996bfa5b6d388c8a7534c985404f7122bba59250
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 05:45:15 -0300

    chore: BookCommand to  IRequest<Domain.Models.Book>

--- src/LocalLibrary.Application/CQRS/Book/Commands/BookCreateCommand.cs
+++ src/LocalLibrary.Application/CQRS/Book/Commands/BookCreateCommand.cs
@@ -1,6 +1,14 @@
-﻿namespace LocalLibrary.Application.CQRS.Book.Commands
+﻿using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Book.Commands
 {
-    public class BookCreateCommand : BookCommand
+    public class BookCreateCommand : IRequest<Domain.Models.Book>
     {
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public string ISBN { get; set; }
+        public Guid AuthorId { get; set; }
+        public Guid GenreId { get; set; }
+        public Guid LanguageId { get; set; }
     }
 }
