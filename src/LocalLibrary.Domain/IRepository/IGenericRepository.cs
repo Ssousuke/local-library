b6039da1622271f6bacf08ee7a8eefbd8efa0098
Ssousuke <wesleysousukee@gmail.com>
2024-01-20 22:44:56 -0300

    rename: contract DeleteByIdAsync

--- src/LocalLibrary.Domain/IRepository/IGenericRepository.cs
+++ src/LocalLibrary.Domain/IRepository/IGenericRepository.cs
@@ -5,7 +5,7 @@
         public Task<T> AddAsync(T entity);
         public Task<T> UpdateAsync(T entity);
         public Task<T> GetByIdAsync(Guid id);
-        public Task<bool> DeleteByIsAsync(Guid id);
+        public Task<bool> DeleteByIdAsync(Guid id);
         public Task<IEnumerable<T>> GetAll();
     }
 }
