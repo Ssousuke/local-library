6590428c375fbdd38706323ddd29a82c61249876
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 05:48:25 -0300

    chore: add Id

--- src/LocalLibrary.Application/CQRS/Genre/Commands/GenreCommand.cs
+++ src/LocalLibrary.Application/CQRS/Genre/Commands/GenreCommand.cs
@@ -4,6 +4,7 @@ namespace LocalLibrary.Application.CQRS.Genre.Commands
 {
     public abstract class GenreCommand : IRequest<Domain.Models.Genre>
     {
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }
