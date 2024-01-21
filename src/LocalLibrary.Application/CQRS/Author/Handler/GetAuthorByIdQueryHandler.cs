f9dcdbc2bfae0453f5467ab6e38f746b96b31e6c
Ssousuke <wesleysousukee@gmail.com>
2024-01-21 02:16:20 -0300

    add author queries handler

---
+++ src/LocalLibrary.Application/CQRS/Author/Handler/GetAuthorByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LocalLibrary.Application.CQRS.Author.Queries;
+using LocalLibrary.Domain.IRepository;
+using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Author.Handler
+{
+    public class GetAuthorByIdQueryHandler : IRequestHandler<GetAuthorByIdQuery, Domain.Models.Author>
+    {
+        private readonly IGenericRepository<Domain.Models.Author> _authorRepository;
+
+        public GetAuthorByIdQueryHandler(IGenericRepository<Domain.Models.Author> authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<Domain.Models.Author> Handle(GetAuthorByIdQuery request, CancellationToken cancellationToken)
+        {
+            return await _authorRepository.GetByIdAsync(request.AuthorId);
+        }
+    }
+}
---
+++ src/LocalLibrary.Application/CQRS/Author/Handler/GetAuthorsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using LocalLibrary.Application.CQRS.Author.Queries;
+using LocalLibrary.Domain.IRepository;
+using MediatR;
+
+namespace LocalLibrary.Application.CQRS.Author.Handler
+{
+    public class GetAuthorsQueryHandler : IRequestHandler<GetAuthorsQuery, IEnumerable<Domain.Models.Author>>
+    {
+        private readonly IGenericRepository<Domain.Models.Author> _authorRepository;
+
+        public GetAuthorsQueryHandler(IGenericRepository<Domain.Models.Author> authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<IEnumerable<Domain.Models.Author>> Handle(GetAuthorsQuery request, CancellationToken cancellationToken)
+        {
+            return await _authorRepository.GetAll();
+        }
+    }
+}
